a6dc6ce84db5240c1fffdb19448e517a0cac616f
proHyundo <guseh08@knou.ac.kr>
2024-10-31 21:06:27 +0900

    init: litis 지원자 백엔드 송현도

---
+++ src/main/java/org/prohyun/litis/LitisApplication.java
@@ -0,0 +1,13 @@
+package org.prohyun.litis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prohyun/litis/LitisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prohyun.litis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
